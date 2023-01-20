13971f9bab816f2587deb88b762e1433c651c51e
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-19 22:35:00 -0300

    ''

--- src/components/Habits.tsx
+++ src/components/Habits.tsx
@@ -1,16 +1,19 @@
-import { TouchableOpacity, Dimensions } from "react-native";
+import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
 
 const weekDays = 7;
 const screenHorizontalPaddins = (32 * 2) / 5;
 export const dayMarginBetween = 8;
 export const daySize = (Dimensions.get('screen').width / weekDays) - (screenHorizontalPaddins + 5);
 
-export function HabitDay() {
-    return(
-        <TouchableOpacity 
+interface Props extends TouchableOpacityProps { };
+
+export function HabitDay({ ...rest }: Props) {
+    return (
+        <TouchableOpacity
             className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
             activeOpacity={0.7}
-            style={{ width: daySize, height: daySize}}
+            style={{ width: daySize, height: daySize }}
+            {...rest}
         />
     )
 }
