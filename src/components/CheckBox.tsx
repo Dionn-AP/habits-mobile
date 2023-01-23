1ec6cbe54946917f7bcbee88cfc0f178addac739
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-23 19:18:09 -0300

    animação do progressbar

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,29 +1,36 @@
-import { Text, View, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Feather } from '@expo/vector-icons';
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated';
 import colors from 'tailwindcss/colors';
 
-interface Props extends TouchableOpacityProps{
+interface Props extends TouchableOpacityProps {
     checked?: boolean;
     title?: string;
+    datePast: boolean;
 }
 
-export function CheckBox({ title, checked = false, ...rest }: Props) {
+export function CheckBox({ title, checked = false, datePast, ...rest }: Props) {
     return (
         <TouchableOpacity
             activeOpacity={0.7}
             className='flex-row mb-2 items-center'
+            disabled={datePast}
             {...rest}
         >
             {
                 checked
                     ?
-                    <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+                    <Animated.View
+                        className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+                        entering={ZoomIn}
+                        exiting={ZoomOut}
+                        >
                         <Feather
                             name='check'
                             size={20}
                             color={colors.white}
                         />
-                    </View>
+                    </Animated.View>
                     :
                     <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
             }
